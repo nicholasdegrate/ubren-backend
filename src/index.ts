7a3c66d5a823eecdf04fd4e311dd1c7a594d6b35
nicholasdegrate <nicholasdegrate@gmail.com>
2021-06-18 09:06:03 -0500

    add script to auto reload the server with changes are made

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import express, { Application, Request, Response, NextFunction } from "express"
 import {createServer} from 'http'
 import { Server, Socket } from 'socket.io'
+
 const app: Application = express()
 const server = createServer(app);
-
-
 const options = {cors: {origin: 'http://localhost:9000'}};
-
 const io = new Server(server, options);
 
 io.on("connect", (socket: Socket) => {
   console.log("user connected", socket.id);
 
-
-  socket.on("chat-msg", function (data) {
+  socket.on("chat-msg", (data) => {
     io.emit("chat-msg", data);
   });
 });
